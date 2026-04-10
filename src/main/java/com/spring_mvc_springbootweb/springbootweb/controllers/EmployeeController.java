5747ccd899baa1a60338d5ec68e03a1e4cd7f5bf
Keval Sardhara <kevalsardhara7@gmail.com>
2026-04-10 18:51:32 +0100

    add(): controller, route, apis, @Anotation added for the controller, path system, dto, entity, h2 database added

--- src/main/java/com/spring_mvc_springbootweb/springbootweb/controllers/EmployeeController.java
+++ src/main/java/com/spring_mvc_springbootweb/springbootweb/controllers/EmployeeController.java
@@ -1,8 +1,42 @@
 package com.spring_mvc_springbootweb.springbootweb.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.spring_mvc_springbootweb.springbootweb.dto.EmployeeDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/v1/employees")
 public class EmployeeController {
 
+    @GetMapping(path = "/message")
+    public String getEmployeeMessage() {
+        return "This is the your unique ID: 999222";
+    }
+
+    @GetMapping(path = "/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeId", required = false) Long id) {
+        return new EmployeeDTO(
+                id,
+                "KEVAL JAMANBHAI SARDHARA",
+                "kevalsardhara9@gmail.com",
+                24,
+                LocalDate.of(2026, 4, 10),
+                true
+        );
+    }
+
+
+    @GetMapping(path = "")
+    public String getAllEmployees(@RequestParam(name = "age", defaultValue = "24", required = false) Integer inputAge,
+                                  @RequestParam(required = false) Integer page_sort) {
+        return new String("HI age is " + inputAge + " - " + page_sort);
+    }
+
+    @PostMapping(path = "/create")
+    public EmployeeDTO createEmployees(@RequestBody EmployeeDTO employeeDTO) {
+        System.out.println(employeeDTO);
+        employeeDTO.setId(120L);
+        return employeeDTO;
+    }
 }
---
+++ src/main/java/com/spring_mvc_springbootweb/springbootweb/entities/EmployeeEntity.java
@@ -0,0 +1,8 @@
+package com.spring_mvc_springbootweb.springbootweb.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class EmployeeEntity {
+
+}
